edefc2a0436a9e7f73a82c23d03eb8509bd05b91
Zaiming (Stone) Shi <zmstone@gmail.com>
2023-04-14 14:55:27 +0200

    Make possible to specify port number from env

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
@@ -14,6 +13,6 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: Number(process.env.PORT) || 5173,
   }
 })
