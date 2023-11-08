827b33e65c2b5991a6286692c3e6c086248fe7f0
Zaiming (Stone) Shi <zmstone@gmail.com>
2023-11-08 09:59:39 +0100

    Refine

--- src/hocon.ts
+++ src/hocon.ts
@@ -1,37 +1,23 @@
-// export type Sidebar = SidebarItem[] | SidebarMulti
-
-// export interface SidebarMulti {
-//   [path: string]: SidebarItem[] | { items: SidebarItem[]; base: string }
-// }
-
-export enum SidebarItemKind {
-  // A config key name.
-  ConfigKey,
-  // A possible type of the config
-  UnionTypeMember,
-}
-
 export type SidebarItem = {
   // The display text of the item.
   text: string
   // The link to expanded details of the item
   link?: string
   // Are the children-items config keys or types?
-  kind?: SidebarItemKind
+  // possible values are 'config-key' or 'type'
+  kind?: string
   // The children of this item.
   items?: SidebarItem[]
 }
 
 export interface Struct {
-  // Display text, e.g. type name
+  // Display text, e.g. type name without namespacing.
   text: string
-  // The link target of this struct
-  // normalized from the full type name
+  // The link-target of this struct.
+  // Normalized from the type full-name
   hash: string
-  // Additional information about the struct
-  label?: { text: string, type: string }[]
-  // More information about the struct
-  info: {attribute: string, specification: string, markdown?: boolean}[]
+  // Markdown format description of the Struct type
+  description?: string
   // All fields of this struct
   fields: StructField[]
 }
@@ -41,79 +27,16 @@ export interface StructField {
   name: string
   // Added new names for the field
   aliases?: string[]
-  // The display text of the field type
-  type: string
-  // The link target of the field's sub-types
-  references?: {type: string, hash: string}[]
-  // More information about the field
-  // default value, description, deprecation, importance
-  info: {attribute: string, specification: string, markdown?: boolean}[]
-}
-
-const exampleData: Data = {
-  sidebar: [
-    {
-      text: 'listeners',
-      link: '#listeners',
-      kind: SidebarItemKind.ConfigKey,
-      items: [
-        {
-          text: 'tcp',
-          link: '#listeners-tcp',
-        }
-      ]
-    },
-    {
-      text: 'authentication[...]',
-      link: '#authentication',
-      kind: SidebarItemKind.UnionTypeMember,
-      items: [
-        {
-          text: 'mongodb',
-          link: '#authentication-mongodb'
-        },
-        {
-          text: 'postgres',
-          link: '#authentication-postgres'
-        }
-      ]
-    }
-  ],
-  structs: [
-    {
-      text: 'Struct1',
-      hash: '#struct1',
-      label: [{ text: 'v5.3+', type: 'info' }, { text: 'experimental', type: 'warning' }],
-      info: [
-        {
-          attribute: 'Description',
-          specification: 'This is a markdown description which supports:<br/>  - HTML tags<br/>  - Markdown syntax',
-          markdown: true,
-        }
-      ],
-      fields: [
-        {
-          name: 'field1',
-          type: 'String',
-          references: [],
-          info: [{ attribute: 'Description', specification: 'This is field 1'},
-                 { attribute: 'Default', specification: 'Default value for field 1'}
-                ]
-        },
-        {
-          name: 'fieldB',
-          aliases: ['field2'],
-          type: "oneOf(Struct2,StructB)",
-          references: [{type: 'Struct2', hash: '#struct2'}, {type: 'StructB', hash: '#structb'}],
-          info: [{ attribute: 'Description', specification: 'This is field 2'}]
-        },
-        {
-          name: 'field3',
-          type: "arrayOf(Struct3)",
-          references: [{type: 'Struct3', hash: '#struct3'}],
-          info: [{ attribute: 'Description', specification: 'This is field 3'}]
-        }
-      ],
-    }
-  ]
+  // Possible types of the field.
+  // Mostly it's a single element array, but in case of union types
+  // it's a list of possible typesa.
+  // When the item is a Struct type, the hash is the link to the Struct
+  types: { type: string; link?: string }[]
+  // Default value
+  default_value?: string
+  // Description of the field
+  description?: string
+  // Importance level of the field, possible values are 'low', 'medium', 'high'.
+  // If not set, it's means 'high' level
+  importance: string
 }
