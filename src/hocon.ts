53ff52cccabd0df213530925f79fd316b6e7e0d0
Zaiming (Stone) Shi <zmstone@gmail.com>
2023-11-21 11:51:40 +0100

    Refactor struct and field types

--- src/hocon.ts
+++ src/hocon.ts
@@ -1,42 +1,41 @@
-export type SidebarItem = {
-  // The display text of the item.
-  text: string
-  // The link to expanded details of the item
-  link?: string
-  // Are the children-items config keys or types?
-  // possible values are 'config-key' or 'type'
-  kind?: string
-  // The children of this item.
-  items?: SidebarItem[]
-}
-
 export interface Struct {
   // Display text, e.g. type name without namespacing.
   text: string
-  // The link-target of this struct.
-  // Normalized from the type full-name
+  // The in-page HTML link anchor of this type
+  // Always starts with 'T-' prefix (for type)
   hash: string
   // Markdown format description of the Struct type
-  description?: string
+  // can be empty string
+  doc: string
   // All fields of this struct
   fields: StructField[]
 }
 
 export interface StructField {
-  // Name of the field
-  name: string
-  // Added new names for the field
+  // The display name of the field
+  text: string
+  // The in-page HTML link anchor of this type
+  // Always starts with 'V-' prefix (for value)
+  hash: string
+  // The display name of the field's typen
+  // It is mostly a short and concise name.
+  // For example:
+  //  - Struct(struct_name1)
+  //  - String("singleton_value")
+  //  - Integer(1..10)
+  //  - Enumm(true,false,quick_deny_anonymous)
+  //  - Array(String)
+  //  - OneOf(String, Integer, Struct(struct_name2)
+  // However, for OneOf types, if there are many types, the display name can be very long.
+  type: string
+  // Doc of the field
+  doc: string
+  // The HOCON-style default value literal of the field
+  // to be wrapped around <code> tag in markdown or HTML
+  default?: string
+  // Aliases of the field
   aliases?: string[]
-  // Possible types of the field.
-  // Mostly it's a single element array, but in case of union types
-  // it's a list of possible typesa.
-  // When the item is a Struct type, the hash is the link to the Struct
-  types: { type: string; link?: string }[]
-  // Default value
-  default_value?: string
-  // Description of the field
-  description?: string
-  // Importance level of the field, possible values are 'low', 'medium', 'high'.
-  // If not set, it's means 'high' level
-  importance: string
+  // Recursively expands the field's struct-type references.
+  // It is usually a single struct, but can be a list of structs for OneOf types.
+  refs: Struct[]
 }
