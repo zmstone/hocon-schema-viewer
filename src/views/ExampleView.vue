b28bc3abf0e473761fff8018751bae7335f3b7cc
zmstone <zmstone@gmail.com>
2025-03-10 17:12:17 +0100

    Refine default prompt

--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -69,7 +69,7 @@ export default defineComponent({
             '- In the schema, the "path" field is a dot-separated string that describes the path to the field from the root of the schema. ' +
             '- The "default" field is the default value of the field if it is not provided. The "required" field is a boolean that describes if the field is required. ' +
             '- The "items" field is an object that describes the items of the field if it is an array. ' +
-            '- When the type is a reference to another schema, generate "Add fields for <reference_name>" to the example. ' +
+            '- When the type is a reference to another schema, generate "# Add fields for <reference_name>" to the example. ' +
             'Below are the requirements for the generated example: ' +
             '- When it is a union type, you should generate an example based on my input after the schema JSON section, if no description is provided, you should generate an example based on the first union member type. ' +
             '- For map, the key is a dollar ($) sign prefixed placehocer such as $name, you should generate a sensible example key for the map based on the path of the field. For example, if the path is "webhook.name", the key should be "mywebhook1".' +
@@ -78,6 +78,8 @@ export default defineComponent({
             '- I prefer to have clean examples, so no need to include comments in the generated example.' +
             '- Do not quote the generated example in triple quotes.' +
             '- If the path has $INDEX in it, it is an array, use array syntax like filed = [...] instead of using index number as key.' +
+            '- The type information is inherited from Eralng type specs, so you should generate the example based on the type specs. For example "binary()" is binary string, etc.' +
+            '- The bytes configs such as "1MB", and duration configs such as "1d" should be quoted. ' +
             '- After generated, go through the requirements and make sure the generated example meets all the above requirements.'
           }, {
             role: 'user',
